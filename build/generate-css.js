fe175f11a1d540d945d296abe502fdfc2e7d00e2
Greg Walker <mgwalker@users.noreply.github.com>
2016-04-26 11:21:10 -0500

    Fixes a bug where reject was undefined

--- build/generate-css.js
+++ build/generate-css.js
@@ -2,7 +2,7 @@
 const fs = require("fs");
 
 module.exports = function(glyphs) {
-	return new Promise(resolve => {
+	return new Promise((resolve, reject) => {
 		const promises = [ ];
 
 		let css = `.fa{display:inline-block;width:1em;height:1em;background-size:contain;}\n.fa-lg{width:1.33333em;height:1.33333em;}\n.fa-2x{width:2em;height:2em;}\n.fa-3x{width:3em;height:3em;}\n.fa-4x{width:4em;height:4em;}\n.fa-5x{width:5em;height:5em;}\n.fa.fa-white{-webkit-filter:brightness(0) invert();-moz-filter:brightness(0) invert();-ms-filter:brightness(0) invert();filter:brightness(0) invert();}\n.fa.fa-black{-webkit-filter:brightness(0);-moz-filter:brightness(0);-ms-filter:brightness(0);filter:brightness(0);}`
